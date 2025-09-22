047b6007a5f5df585b5e3117210d42dbdc2646ff
Philip Lindskog <philip_lindskog@hotmail.com>
2025-09-22 12:24:47 +0200

    Created SessionController with GetAllAsync method. Updated ISessionsService with  GetAllSessionsAsync method

---
+++ Presentation/Controllers/SessionController.cs
@@ -0,0 +1,23 @@
+﻿using Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class SessionController(ISessionService sessionService) : Controller
+    {
+        private readonly ISessionService _sessionService = sessionService;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var sessions = await _sessionService.GetAllSessionsAsync();
+
+            return Ok(sessions);
+        }
+    }
+}
