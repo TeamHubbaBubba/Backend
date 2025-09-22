51420c6684c91de27fb76c95c84bd5d0277c83cb
Max Natanaelsson <max.natanaelsson@outlook.com>
2025-09-22 12:20:20 +0200

    Added SessionFactory.

--- Business/Factories/SessionFactory.cs
+++ Business/Factories/SessionFactory.cs
@@ -1,5 +1,23 @@
 ﻿
+using Business.Dtos;
+using Data.Entities;
+
 namespace Business.Factories;
 public class SessionFactory
 {
+    public static SessionEntity ToEntity(SessionDto dto)
+    {
+
+        var entity = new SessionEntity
+        {
+            Title = dto.Title,
+            Description = dto.Description,
+            MaxParticipants = dto.MaxParticipants,
+            CurrentParticipants = dto.CurrentParticipants,
+            Date = dto.Date
+        };
+
+        return entity;
+    }
+
 }
