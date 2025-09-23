552fc1138475ff9bd70b74d01f9c903fe432be79
Carthur83 <christian_A_83@hotmail.com>
2025-09-23 07:16:41 +0200

    Add DeleteAsync in controller

---
+++ Presentation/Controllers/SessionsController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SessionsController(ISessionService sessionService) : ControllerBase
+{
+    private readonly ISessionService _sessionService = sessionService;
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(string id)
+    {
+        var result = await _sessionService.DeleteSessionAsync(x => x.Id == id);
+
+        if (!result.Success)
+            return StatusCode(result.StatusCode, new { message = result.ResultMessage });
+
+        return Ok();
+    }
+}
