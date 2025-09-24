9f82f8842859e89162857286d209a627f3535da6
asaj1 <asa.johansson12@learnet.se>
2025-09-24 10:11:21 +0200

    Moved httprequest from sessionController to SessionsController and deleted sessionController.

--- Presentation/Controllers/SessionsController.cs
+++ Presentation/Controllers/SessionsController.cs
@@ -1,4 +1,6 @@
-﻿using Business.Interfaces;
+﻿using Business.Dtos;
+using Business.Interfaces;
+using Business.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
@@ -15,6 +17,20 @@ namespace Presentation.Controllers
             var sessions = await _sessionService.GetAllSessionsAsync();
 
             return sessions.Success ? Ok(sessions) : NotFound("Inga träningspass tillgängliga");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] SessionDto form)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var createdSession = await _sessionService.CreateSessionAsync(form);
+
+            return createdSession.Success
+                ? Ok(createdSession.Success)
+                : BadRequest();
         }
     }
 }
