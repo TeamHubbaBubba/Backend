2c93a65710dd1de29abc6ea7e257ce3a6a56b1f3
Emil Begovic <emil@begovic.se>
2025-09-29 12:46:18 +0200

    Integrate ASP.NET Identity with custom UserEntity
    Refactored DataContext to inherit from IdentityDbContext and seed default roles. Added UserEntity class extending IdentityUser with first and last name properties. Configured Identity services and authentication in Program.cs to support user management and role-based authorization.

---
+++ Data/Entities/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities
+{
+    [Table("Users")]
+    public class UserEntity : IdentityUser<Guid>
+    {
+        [ProtectedPersonalData]
+        public string FirstName { get; set; } = null!;
+        [ProtectedPersonalData]
+        public string LastName { get; set; } = null!;
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,11 +1,12 @@
 using Data.Contexts;
 using Business.Interfaces;
 using Business.Services;
-
 using Microsoft.EntityFrameworkCore;
 using Data.Interfaces;
 using Data.Repositories;
 using Business.Models;
+using Data.Entities;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,29 @@ builder.Services.AddSwaggerGen();
 
 //builder.Services.AddScoped<DataContext>();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+
+builder.Services.AddIdentity<UserEntity, IdentityRole>(options => 
+{
+    options.SignIn.RequireConfirmedEmail = false;
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 5;
+
+})
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/api/auth/login";
+    options.AccessDeniedPath = "/api/auth/access-denied"; 
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    options.SlidingExpiration = true;
+});
+
 
 //Här lägger vi repositories
 builder.Services.AddScoped<ISessionRepository, SessionRespository>();
@@ -38,6 +62,7 @@ app.UseCors( x =>
     x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
 );
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
