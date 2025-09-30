aa073e552caa36d2b687af43a433f35577a76cb4
asaj1 <asa.johansson12@learnet.se>
2025-09-30 10:48:47 +0200

    Update program.cs to implement Guid based roles. Add empty AuthController. Add interface and repository for auth and add signout method.

---
+++ Data/Repositories/AuthRepository.cs
@@ -0,0 +1,30 @@
+﻿using Data.Entities;
+using Data.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class AuthRepository(SignInManager<UserEntity> signInManager): IAuthRepository
+    {
+        private readonly SignInManager<UserEntity> _signInManager = signInManager;
+
+        public async Task<bool> SignOutAsync()
+        {
+            try
+            {
+                await _signInManager.SignOutAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error signing out: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
