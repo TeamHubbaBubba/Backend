e82e9ed1588e97b5ab84f0264be2fe51b871cca4
MSPlearns <mspinolapulido@gmail.com>
2025-10-01 11:16:31 +0200

    Unit testing för BookingService - Create method. Added nullable to Create method in BaseRepo and its interface.
    Updated the `CreateAsync` method in `IBaseRepository` and `BaseRepository` to return a nullable entity.
    
    Added `BookingService_Test` with unit tests for `CreateBookingAsync` to validate behavior for various scenarios, including null input, repository failures, and exceptions.

--- Test/Business/BookingService_Test.cs
+++ Test/Business/BookingService_Test.cs
@@ -1,5 +1,6 @@
 ﻿
 using Business.Interfaces;
+using Business.Services;
 using Data.Interfaces;
 using Moq;
 
@@ -9,4 +10,88 @@ public class BookingService_Test
 {
     private readonly Mock<IBookingRepository> _repositoryMock;
     private readonly IBookingService _bookingService;
+    public BookingService_Test()
+    {
+        _repositoryMock = new Mock<IBookingRepository>();
+        _bookingService = new BookingService(_repositoryMock.Object);
+    }
+
+    //Create
+
+    //Create should return bad request when sessionId is null
+    [Fact]
+    public async Task CreateBooking_ShouldReturnBadRequest_WhenSessionIdIsNull()
+    {
+        //Arrange
+        string sessionId = null;
+        Guid userId = Guid.NewGuid();
+
+        // Act
+        var result = await _bookingService.CreateBookingAsync(sessionId!, userId);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.Equal(400, result.StatusCode);
+
+    }
+    //Create should return error when repository returns null
+    [Fact]
+    public async Task CreateBooking_ShouldReturnError_WhenRepoReturnsNull()
+    {
+        //Arrange
+        string sessionId = Guid.NewGuid().ToString();
+        Guid userId = Guid.NewGuid();
+        _repositoryMock
+            .Setup(r => r.CreateAsync(It.IsAny<Data.Entities.BookingEntity >()))
+            .ReturnsAsync((Data.Entities.BookingEntity?)null);
+
+        // Act
+
+        var result = await _bookingService.CreateBookingAsync(sessionId, userId);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.Equal(500, result.StatusCode);
+
+    }
+    //Create should return ok when repository returns a booking
+    [Fact]
+    public async Task CreateBooking_ShouldReturnOk_WhenRepoReturnsBooking()
+    {
+        //Arrange
+        string sessionId = Guid.NewGuid().ToString();
+        string bookingId = Guid.NewGuid().ToString();
+        Guid userId = Guid.NewGuid();
+        _repositoryMock
+            .Setup(r => r.CreateAsync(It.IsAny<Data.Entities.BookingEntity>()))
+            .ReturnsAsync(new Data.Entities.BookingEntity { Id = bookingId, SessionId = sessionId, UserId = userId });
+        // Act
+        var result = await _bookingService.CreateBookingAsync(sessionId, userId);
+
+        // Assert
+        Assert.True(result.Success);
+        Assert.Equal(200, result.StatusCode);
+
+    }
+    //Create should return error when exception is thrown
+    [Fact]
+    public async Task CreateBooking_ShouldReturnError_WhenAnExceptionisThrown()
+    {
+        //Arrange
+        string sessionId = Guid.NewGuid().ToString();
+        Guid userId = Guid.NewGuid();
+        _repositoryMock
+            .Setup(r => r.CreateAsync(It.IsAny<Data.Entities.BookingEntity>()))
+            .ThrowsAsync(new Exception());
+        // Act
+        var result = await _bookingService.CreateBookingAsync(sessionId, userId);
+        // Assert
+        Assert.False(result.Success);
+        Assert.Equal(500, result.StatusCode);
+
+    }
+
+    //Read
+    //Update
+    //Delete
 }
