69b1d7d6cfb8d98d488af502ea595ad536b30cbd
Carthur83 <christian_A_83@hotmail.com>
2025-10-02 07:45:16 +0200

    Add BookingService and GetBookedSessionsByUserId

---
+++ Business/Services/BookingService.cs
@@ -0,0 +1,23 @@
+﻿using Business.Factories;
+using Business.Interfaces;
+using Business.Models;
+using Data.Interfaces;
+
+namespace Business.Services;
+
+public class BookingService(IBookingRepository bookingRepository) : IBookingService
+{
+    private readonly IBookingRepository _bookingRepository = bookingRepository;
+
+    public async Task<ResponseResult> GetBookedSessionsByUserIdAsync(Guid userId)
+    {
+        var sessionEntities = await _bookingRepository.GetBookingsByUserIdAsync(userId);
+
+        if (!sessionEntities.Any())
+            return ResponseResult.NotFound("No booked sessions found");
+
+        var bookedSessions = sessionEntities.Select(SessionFactory.EntityToModel);
+
+        return ResponseResult<IEnumerable<SessionModel>>.Ok(bookedSessions);
+    }
+}
